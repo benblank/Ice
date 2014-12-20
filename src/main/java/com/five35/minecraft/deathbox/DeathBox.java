967abf5dce413b202ef07a3907fed569a6e3b43e
Ben Blank <ben.blank@gmail.com>
2014-12-19 23:11:22 -0800

    add mod class skeleton

---
+++ src/main/java/com/five35/minecraft/deathbox/DeathBox.java
@@ -0,0 +1,29 @@
+package com.five35.minecraft.deathbox;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.EventHandler;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import org.apache.logging.log4j.Logger;
+
+@Mod(modid = "DeathBox")
+public class DeathBox {
+	Logger logger;
+
+	@EventHandler
+	public void init(final FMLInitializationEvent event) {
+		this.logger.debug("Registering event handler.");
+		// TODO: register event handler
+	}
+
+	@EventHandler
+	public void preInit(final FMLPreInitializationEvent event) {
+		this.logger = event.getModLog();
+
+		this.logger.debug("Registering block.");
+		// TODO: register block
+
+		this.logger.debug("Reading config.");
+		// TODO: config
+	}
+}
