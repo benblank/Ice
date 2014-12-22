cef26d182a175056623bca18662efd5566b68985
Ben Blank <ben.blank@gmail.com>
2014-12-22 00:05:46 -0800

    add null-checking annotations

--- src/main/java/com/five35/minecraft/deathbox/inventorymanager/InventoryManager.java
+++ src/main/java/com/five35/minecraft/deathbox/inventorymanager/InventoryManager.java
@@ -2,15 +2,19 @@ package com.five35.minecraft.deathbox.inventorymanager;
 
 import java.util.List;
 import java.util.Map;
+import javax.annotation.Nonnull;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
 
 public interface InventoryManager {
 	void clearSlots();
 
+	@Nonnull
 	List<ItemStack> fillSlots(final EntityPlayer player, final Map<Integer, ItemStack> slots);
 
+	@Nonnull
 	String getName();
 
+	@Nonnull
 	Map<Integer, ItemStack> getSlots(final EntityPlayer player);
 }
---
+++ src/main/java/com/five35/minecraft/deathbox/inventorymanager/package-info.java
@@ -0,0 +1,5 @@
+@ParametersAreNonnullByDefault
+package com.five35.minecraft.deathbox.inventorymanager;
+
+import javax.annotation.ParametersAreNonnullByDefault;
+
