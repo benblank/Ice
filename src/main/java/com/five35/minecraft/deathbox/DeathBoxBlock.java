935db1b153e29945b3170835d2c26d78adb37296
Ben Blank <ben.blank@gmail.com>
2015-06-24 20:48:27 -0700

    update block for 1.8

--- src/main/java/com/five35/minecraft/deathbox/DeathBoxBlock.java
+++ src/main/java/com/five35/minecraft/deathbox/DeathBoxBlock.java
@@ -3,11 +3,14 @@ package com.five35.minecraft.deathbox;
 import com.mojang.authlib.GameProfile;
 import net.minecraft.block.Block;
 import net.minecraft.block.ITileEntityProvider;
+import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.AxisAlignedBB;
+import net.minecraft.util.BlockPos;
+import net.minecraft.util.EnumFacing;
 import net.minecraft.util.MovingObjectPosition;
 import net.minecraft.world.Explosion;
 import net.minecraft.world.IBlockAccess;
@@ -22,17 +25,17 @@ public class DeathBoxBlock extends Block implements ITileEntityProvider {
 	}
 
 	@Override
-	public boolean canEntityDestroy(final IBlockAccess world, final int x, final int y, final int z, final Entity entity) {
+	public boolean canEntityDestroy(final IBlockAccess world, final BlockPos position, final Entity entity) {
 		return false;
 	}
 
 	@Override
-	public boolean canHarvestBlock(final EntityPlayer player, final int meta) {
+	public boolean canHarvestBlock(final IBlockAccess world, final BlockPos position, final EntityPlayer player) {
 		return false;
 	}
 
 	@Override
-	public boolean canPlaceBlockAt(final World world, final int x, final int y, final int z) {
+	public boolean canPlaceBlockAt(final World world, final BlockPos position) {
 		return false;
 	}
 
@@ -42,12 +45,12 @@ public class DeathBoxBlock extends Block implements ITileEntityProvider {
 	}
 
 	@Override
-	public AxisAlignedBB getCollisionBoundingBoxFromPool(final World world, final int x, final int y, final int z) {
+	public AxisAlignedBB getCollisionBoundingBox(final World world, final BlockPos position, final IBlockState state) {
 		return null;
 	}
 
 	@Override
-	public ItemStack getPickBlock(final MovingObjectPosition target, final World world, final int x, final int y, final int z) {
+	public ItemStack getPickBlock(final MovingObjectPosition target, final World world, final BlockPos position) {
 		return null;
 	}
 
@@ -62,19 +65,19 @@ public class DeathBoxBlock extends Block implements ITileEntityProvider {
 	}
 
 	@Override
-	public boolean onBlockActivated(final World world, final int x, final int y, final int z, final EntityPlayer player, final int side, final float xOffset, final float yOffset, final float zOffset) {
+	public boolean onBlockActivated(final World world, final BlockPos position, final IBlockState state, final EntityPlayer player, final EnumFacing side, final float xOffset, final float yOffset, final float zOffset) {
 		if (world.isRemote) {
 			return false;
 		}
 
-		final TileEntity tileEntity = world.getTileEntity(x, y, z);
+		final TileEntity tileEntity = world.getTileEntity(position);
 
 		if (tileEntity instanceof DeathBoxTileEntity) {
 			final DeathBoxTileEntity deathBox = (DeathBoxTileEntity) tileEntity;
 			final GameProfile owner = deathBox.getOwner();
 
 			if (DeathBox.getProxy().getConfig().canRecover(owner, player)) {
-				final String message = String.format("%s is recovering a death box left by %s.", owner.getName(), player.getCommandSenderName());
+				final String message = String.format("%s is recovering a death box left by %s.", owner.getName(), player.getCommandSenderEntity().getName());
 				DeathBox.getProxy().getLogger().info(message);
 
 				deathBox.recover(player);
@@ -82,7 +85,7 @@ public class DeathBoxBlock extends Block implements ITileEntityProvider {
 				return true;
 			}
 
-			final String message = String.format("%s does not have permission to recover a death box left by %s.", owner.getName(), player.getCommandSenderName());
+			final String message = String.format("%s does not have permission to recover a death box left by %s.", owner.getName(), player.getCommandSenderEntity().getName());
 			DeathBox.getProxy().getLogger().info(message);
 		}
 
@@ -90,41 +93,36 @@ public class DeathBoxBlock extends Block implements ITileEntityProvider {
 	}
 
 	@Override
-	public void onBlockClicked(final World world, final int x, final int y, final int z, final EntityPlayer player) {
+	public void onBlockClicked(final World world, final BlockPos position, final EntityPlayer player) {
 		if (world.isRemote) {
 			return;
 		}
 
-		final TileEntity tileEntity = world.getTileEntity(x, y, z);
+		final TileEntity tileEntity = world.getTileEntity(position);
 
 		if (tileEntity instanceof DeathBoxTileEntity) {
 			final DeathBoxTileEntity deathBox = (DeathBoxTileEntity) tileEntity;
 			final GameProfile owner = deathBox.getOwner();
 
 			if (DeathBox.getProxy().getConfig().canPop(owner, player)) {
-				final String message = String.format("%s is popping a death box left by %s.", owner.getName(), player.getCommandSenderName());
+				final String message = String.format("%s is popping a death box left by %s.", owner.getName(), player.getCommandSenderEntity());
 				DeathBox.getProxy().getLogger().info(message);
 
 				deathBox.pop();
 			} else {
-				final String message = String.format("%s does not have permission to pop a death box left by %s.", owner.getName(), player.getCommandSenderName());
+				final String message = String.format("%s does not have permission to pop a death box left by %s.", owner.getName(), player.getCommandSenderEntity());
 				DeathBox.getProxy().getLogger().info(message);
 			}
 		}
 	}
 
 	@Override
-	public void onBlockExploded(final World world, final int x, final int y, final int z, final Explosion explosion) {
+	public void onBlockExploded(final World world, final BlockPos position, final Explosion explosion) {
 		// DeathBox is unexplodable!
 	}
 
 	@Override
-	public boolean removedByPlayer(final World world, final EntityPlayer player, final int x, final int y, final int z) {
-		return false;
-	}
-
-	@Override
-	public boolean renderAsNormalBlock() {
+	public boolean removedByPlayer(final World world, final BlockPos position, final EntityPlayer player, final boolean willHarvest) {
 		return false;
 	}
 }
