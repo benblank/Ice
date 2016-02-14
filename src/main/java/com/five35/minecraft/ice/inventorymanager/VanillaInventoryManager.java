421020e64534ed22daf61ac9e1b1f89ad56c0fb7
Ben Blank <ben.blank@gmail.com>
2016-02-13 17:11:51 -0800

    explicitly sync player inventory after filling

--- src/main/java/com/five35/minecraft/ice/inventorymanager/VanillaInventoryManager.java
+++ src/main/java/com/five35/minecraft/ice/inventorymanager/VanillaInventoryManager.java
@@ -1,9 +1,26 @@
 package com.five35.minecraft.ice.inventorymanager;
 
+import java.util.List;
+import java.util.Map;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.inventory.IInventory;
+import net.minecraft.item.ItemStack;
 
 public class VanillaInventoryManager extends GeneralInventoryManager {
+	@Override
+	public List<ItemStack> fillSlots(EntityPlayer player, Map<Integer, ItemStack> slots) {
+		List<ItemStack> leftovers = super.fillSlots(player, slots);
+
+		// Simply marking the player's inventory as dirty doesn't actually
+		// cause the server to synchronize it to the client.
+		if (player instanceof EntityPlayerMP) {
+			((EntityPlayerMP) player).sendContainerToPlayer(player.inventoryContainer);
+		}
+
+		return leftovers;
+	}
+
 	@Override
 	protected IInventory getInventory(EntityPlayer player) {
 		return player.inventory;
