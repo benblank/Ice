0ed0458b8127038397402fb95fbdff57c0d39126
Ben Blank <ben.blank@gmail.com>
2016-02-15 22:30:04 -0800

    add updater support

--- src/main/java/com/five35/minecraft/ice/Ice.java
+++ src/main/java/com/five35/minecraft/ice/Ice.java
@@ -7,7 +7,7 @@ import net.minecraftforge.fml.common.SidedProxy;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
-@Mod(modid = "Ice", dependencies = "after:Baubles")
+@Mod(modid = "Ice", dependencies = "after:Baubles", updateJSON = "https://github.com/benblank/Ice/blob/master/update.json")
 public class Ice {
 	@Instance
 	private static Ice instance;
